aa72eb5bc58bb6728d508a8fb0125151003ae990
Alex Sandiiarov <sandiyarov@gmail.com>
2019-02-02 19:58:49 -0600

    fix

--- packages/graphql-gateway/src/index.ts
+++ packages/graphql-gateway/src/index.ts
@@ -2,11 +2,11 @@ import Gateway from './gateway';
 
 const gateway = new Gateway({
   user: {
-    definition: require.resolve('./schema/user.graphql'),
+    definition: './src/schema/user.graphql',
     endpoint: 'http://localhost:4001/graphql',
   },
   post: {
-    definition: require.resolve('./schema/post.graphql'),
+    definition: './src/schema/post.graphql',
     endpoint: 'http://localhost:4002/graphql',
   },
 });
