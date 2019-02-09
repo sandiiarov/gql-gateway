b8966f44b0ee6d672697d3a566d9dad03b11aca6
Alex Sandiiarov <sandiyarov@gmail.com>
2019-02-09 17:09:46 -0600

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
+import config from '../config.json';
 import Gateway from './tools/gateway';
 
-export const gateway = new Gateway({
-  user: {
-    definition: './schema/user.graphql',
-    endpoint: 'http://localhost:4001/graphql',
-  },
-  post: {
-    definition: './schema/post.graphql',
-    endpoint: 'http://localhost:4002/graphql',
-  },
-  link: {
-    definition: './schema/link.graphql',
-  },
-});
+export const gateway = new Gateway(config);
 
 export async function run() {
   const { url } = await gateway.listen(4000);
